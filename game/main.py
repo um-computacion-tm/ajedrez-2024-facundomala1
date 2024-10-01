24f91eb4842f6aede30ca8073d071c202466974e
facundomala1 <f.mala@alumno.um.edu.ar>
2024-09-30 22:55:01 -0300

    main

--- game/main.py
+++ game/main.py
@@ -1,43 +1,37 @@
-from exeptions import CasillaOcupada, PiezaNoExiste, MismaCasilla, ColorIncorrecto, MovimientoInvalido, IndexErrorPersonalizada
-from cli import Cliente
+from game.exeptions import *
+from game.cli import Cliente
 
 def main():
 
     cliente = Cliente()
-    iniciar = cliente.inicializar_juego()
+    iniciar = cliente.inicializar_chess()
 
     while True:
-
         cliente.comenzar_iteracion(iniciar[0], iniciar[1])
-
         try:
             
-            input = cliente.input_function()
+            input = cliente.funcion_entrada()
 
             if input == False:
                 break
             
-            movement = iniciar[0].move(input[0], input[1], input[2], input[3])
+            movimiento = iniciar[0].move(input[0], input[1], input[2], input[3])
 
-            # Si el rey ha sido eliminado, terminamos la partida
-            if movement == "ReyEliminado":
+            if movimiento == "ReyEliminado":
                 print("El rey ha sido eliminado. La partida ha terminado.")
                 print("El ganador es: " + iniciar[0].get_ganador())
                 break
 
-            # Limpiar consola por cada iteracion
-            cliente.clear_console()
+            cliente.limpiar_consola()
 
-            # Si el movimiento es valido, nos ahorramos las Excepciones
-            if movement == "Valido":
+            if movimiento == "Valido":
                 continue
 
-            cliente.detectar_exepcion(movement)
+            cliente.detectar_excepcion(movimiento)
 
-        #Aca atrapamos TODOS los errores
         except ValueError as e:
             print("El valor introducido no es un entero. Intentelo de nuevo.")
-        except (IndexErrorPersonalizada, CasillaOcupada, PiezaNoExiste, MismaCasilla, ColorIncorrecto, MovimientoInvalido) as e:
+        except (CasillaOcupada, PiezaNoExiste,IndexErrorPersonalizada, MismaCasilla, ColorIncorrecto, InvalidMove) as e:
             print(e)
 
 
