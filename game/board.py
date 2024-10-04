e70eca26478646d0d82291f750feaf8ffdbd0c1d
facundomala1 <f.mala@alumno.um.edu.ar>
2024-10-04 10:25:25 -0300

    final tablero

--- game/board.py
+++ game/board.py
@@ -1,18 +1,20 @@
-from rook import Rook
-from knight import Knight
-from bishop import Bishop
-from king import King
-from queen import Queen
-from pawn import Pawn
+from game.rook import Rook
+from game.knight import Knight
+from game.bishop import Bishop
+from game.king import King
+from game.queen import Queen
+from game.pawn import Pawn
 class Board:
     def __init__(self):
+        # Positions, va a ser una matriz de 8 filas y 8 columnas
+        # Instanciamos una matriz vacia e insertamos las piezas en sus posiciones iniciales.
         self.__positions__ = []
-        for _ in range(8):
+        for fila in range(8):
             col = []
-            for _ in range(8):
+            for columna in range(8):
                 col.append(None)
-            self.___positions__.append(col)
-            
+            self.__positions__.append(col)
+
         self.__positions__[0][0] = Rook("Rook","White")
         self.__positions__[0][1] = Knight("Knight","White")
         self.__positions__[0][2] = Bishop("Bishop","White")
@@ -21,6 +23,7 @@ class Board:
         self.__positions__[0][5] = Bishop("Bishop","White")
         self.__positions__[0][6] = Knight("Knight","White")
         self.__positions__[0][7] = Rook("Rook","White")
+
         for i in range(8):
             self.__positions__[1][i] = Pawn("Pawn","White")
             self.__positions__[6][i] = Pawn("Pawn","Black")
@@ -32,13 +35,17 @@ class Board:
         self.__positions__[7][4] = Queen("Queen","Black")
         self.__positions__[7][5] = Bishop("Bishop","Black")
         self.__positions__[7][6] = Knight("Knight","Black")
-        self.__positions__[7][7] = Rook("Rook","Black")    
-    def get_positions(self, row, col):
-        return self.__positions__[row][col]
+        self.__positions__[7][7] = Rook("Rook","Black")
+
+    def get_positions(self):
+        return self.__positions__
     def set_positions(self, from_row, from_col, to_row, to_col):
+        # Copiamos la pieza de la casilla origen a la casilla destino
         self.__positions__[to_row][to_col] = self.__positions__[from_row][from_col]
+        # Eliminamos la pieza de la casilla origen
         self.__positions__[from_row][from_col] = None
+
     def get_piece(self, row, col):
         if self.__positions__[row][col] is None:
-            return None
+            return None # Retornamos None si la casilla no tiene pieza
         return self.__positions__[row][col]
